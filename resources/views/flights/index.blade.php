d5a02b4e88d6c294be662597fb5c3dc323c4e814
niculescu-madalin <116899982+niculescu-madalin@users.noreply.github.com>
2025-06-12 13:55:41 +0300

    Changed redirect to login

--- resources/views/flights/index.blade.php
+++ resources/views/flights/index.blade.php
@@ -189,10 +189,7 @@
                                         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Buy</button>
                                     </form>
                                     @else
-                                    <form method="GET" action="{{ route('login') }}">
-                                        <input type="hidden" name="redirect_to" value="{{ route('flights.pay') }}?flight_id={{ $flight->id }}&amount={{ $flight->price }}">
-                                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Buy</button>
-                                    </form>
+                                    <a href="{{ route('login') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Buy</a>
                                     @endauth
                                 </div>
                             </details>
